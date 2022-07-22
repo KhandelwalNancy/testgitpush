e708248eb2d75de540b169c116a150afaa0646a1
Nancy <nancykhandlwal27@gmailcom>
2022-07-22 23:13:49 +0530

    this is im trying 2nd time

--- test2.py
+++ test2.py
@@ -11,6 +11,12 @@ d = {
      "surname" : "kumar"
 }
 
+d = {
+    "name" : "sudhanshu" ,
+    "email" : "sudhanshu@ineuron.ai" ,
+     "surname" : "kumar"
+}
+
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d )
